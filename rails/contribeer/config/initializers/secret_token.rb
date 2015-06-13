b36232700450ccd55b0ba6dcc27505adcd1919c4
daichi <daipanchi@gmail.com>
2015-06-13 18:11:51 +0900

    First commit

---
+++ rails/contribeer/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Contribeer::Application.config.secret_key_base = '75d662ff527ff08fb47b25a7c8a9bd5875ccd2bdcf2a9050a1f6e346e58ec5597a9936cabc06e898e0d3172a4fbefe8c3350a95bb5823a210613f90107858116'
---
+++ rails/contribeer/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Contribeer::Application.config.session_store :cookie_store, key: '_contribeer_session'
